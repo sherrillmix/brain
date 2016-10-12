97d1c08630d058a870300beb54f7b26043d068ba
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-10-12 09:44:17 -0400

    Remove done with parallel

--- blatHuman.bash
+++ blatHuman.bash
@@ -8,7 +8,7 @@ for ii in data/*R[12]*.fastq.gz;do
   if [ ! -e "$out" ];then
     echo Running blat on $ii $(date)
     sem --no-notice -j15 "zcat $ii|awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,\">\");print}; if(P==4)P=0; P++}'|~/installs/blat/blat ~/installs/blat/hg38.2bit stdin >(gzip>$out)"
-    echo Done with blat on $ii $(date)
+    #echo Done with blat on $ii $(date)
   else
     echo File already exists. Skipping
   fi
