b040a2d4baafc3f335a3faec08bfe9d81137d0bc
sherrillmix <sherrillmix@users.noreply.github.com>
2016-11-07 15:22:16 -0500

    Limit to 100 hits

--- blastNt.bash
+++ blastNt.bash
@@ -7,7 +7,7 @@ for ii in work/trim*R[12]*.fastq.gz;do
   out=work/blast_${base%.fastq.gz}.blast.gz
   if [ ! -e "$out" ];then
     echo Running blast on $ii $(date)
-    zcat $ii|awk 'BEGIN{P=1}{if(P==1||P==2){gsub("^@",">");print}; if(P==4)P=0; P++}'|parallel --block 100k  -L2 -j4 --pipe blastn -db ~/db/nt/nt -outfmt 6 -num_threads 5 -query - |gzip>$out
+    zcat $ii|awk 'BEGIN{P=1}{if(P==1||P==2){gsub("^@",">");print}; if(P==4)P=0; P++}'|parallel --block 100k  -L2 -j4 --pipe blastn -db ~/db/nt/nt -outfmt 6 -num_threads 5 -max_target_seqs 100 -query - |gzip>$out
   else
     echo File already exists. Skipping
   fi
